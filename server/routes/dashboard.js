319749d1cbeb2cbbeeb3677d2ac130891c2ea037
0Phonlakit <phonlakit.lila@kmutt.ac.th>
2025-05-18 16:16:51 +0700

    debug admin verify routes

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,10 +2,9 @@ const express = require("express");
 const {
     getDashboardOverview,
 } = require("../controllers/admindashboardController");
-const { verifyToken } = require("../middlewares/auth"); 
 
 const router = express.Router();
 
-router.get("/getAdminDashboard", verifyToken, getDashboardOverview);
+router.get("/getAdminDashboard", getDashboardOverview);
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -9,16 +9,16 @@ const {
   getRoleByUserId,
   getPersonalInfo
 } = require("../controllers/userManagement"); 
-const { verifyToken, verifyRole } = require("../middlewares/auth"); 
+const { verifyToken } = require("../middlewares/auth"); 
 
 const router = express.Router();
 
 router.get("/", getApiMessage);
-router.get("/getAllUsers", verifyToken, verifyRole(["Admin"]), getAllUsers);
-router.post("/createUser", verifyToken, verifyRole(["Admin"]), createUser);
-router.get("/getUser/:id", verifyToken, verifyRole(["Admin"]), getUserById);
-router.put("/updateUser/:id", verifyToken, verifyRole(["Admin"]), updateUser);
-router.delete("/deleteUser/:id", verifyToken, verifyRole(["Admin"]), deleteUser);
+router.get("/getAllUsers", verifyToken, getAllUsers);
+router.post("/createUser", verifyToken, createUser);
+router.get("/getUser/:id", verifyToken, getUserById);
+router.put("/updateUser/:id", verifyToken, updateUser);
+router.delete("/deleteUser/:id", verifyToken, deleteUser);
 router.get("/user/personal", verifyToken, getPersonalInfo);
 router.get("/getRoleByUser", verifyToken, getRoleByUserId);
 
